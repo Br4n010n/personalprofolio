3bd177c2fd9b7d26fc1e9b41ee302f13bc716f06
Br4n010n <114452105+Br4n010n@users.noreply.github.com>
2022-12-12 18:43:23 -0700

    Lost in Space, can't get planets to work

--- planets/sw-planets.js
+++ planets/sw-planets.js
@@ -1,4 +1,4 @@
-import { planets } from '../data/planets.js';
+import { planets } from'../data/planets.js';
 
 const nav = document.querySelector('.nav')
 const navList = document.querySelector('.navList')
@@ -7,7 +7,9 @@ const planetsholo = document.querySelector ('.planetsholo')
 function populateNav() {
     planets.forEach(planets) 
     let listItem = document.createElement('li')
+   listItem.textContent = planet.name
     navList.appendChild(listItem)
+
 
 
 
