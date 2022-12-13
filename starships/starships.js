7083d1e4caaa2c92898f6a8729f9e036e9e274ee
Br4n010n <114452105+Br4n010n@users.noreply.github.com>
2022-12-12 21:36:41 -0700

    I think that commited??

---
+++ starships/starships.js
@@ -0,0 +1,44 @@
+import { starships } from '../data/starships.js'
+import { removeChildren, getLastNumber } from '../utils/index.js'
+const nav = document.querySelector('nav')
+const navList = document.querySelector('.navList')
+const shipViewer = document.querySelector('.shipViewer')
+const modal = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+
+closeButton.addEventListener('click', () => {
+    modal.classList.toggle('is-active')
+})
+
+function populateNav() {
+    starships.forEach((starship) => {
+        let anchor = document.createElement('a')
+        anchor.textContent = starship.name
+        anchor.href = '#'
+
+        anchor.addEventListener('click', () => populateShipView(starship))
+
+        let listItem = document.createElement('li')
+        listItem.appendChild(anchor)
+        navList.appendChild(listItem)
+    })
+}
+function populateShipView(shipData) {
+    
+    removeChildren(shipViewer)
+    const shipImage = document.createElement('img')
+    let shipNum = getLastNumber(shipData.url)
+
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+
+    shipImage.addEventListener('error', () => {
+        console.log('We got an image loading error!')
+        shipImage.hidden = true
+        modal.classList.toggle('is-active')
+    })
+
+    shipViewer.appendChild(shipImage)
+}
+
+
+populateNav()
--- utils/index.js
+++ utils/index.js
@@ -3,6 +3,12 @@ export function removeChildren(container) {
         container.removeChild(container.firstChild)
       }
 }
+
 export function getLastNumber(url) {
     let end = url.lastIndexOf('/')
     let start = end - 2
+    if (url.charAt(start) === '/') {
+      start++
+    }
+    return url.slice(start, end)
+  }
